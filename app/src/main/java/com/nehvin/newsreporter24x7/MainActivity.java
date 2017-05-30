ccf32291dc0262b1829f32c891018f9b170a3d56
reachvineetjain <vkjain@gmail.com>
2017-05-30 08:27:36 +0530

    News reporter
    An empty application. Need to create a news reporter app from various
    network resources available on the the internet.
    More specifically using the PaperBoy APIs

---
+++ app/src/main/java/com/nehvin/newsreporter24x7/MainActivity.java
@@ -0,0 +1,16 @@
+package com.nehvin.newsreporter24x7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+// Visit the below 2 links for what needs to be done on the app
+// https://classroom.udacity.com/courses/ud843/lessons/6752095343239847/concepts/78222105900923
+// https://review.udacity.com/#!/rubrics/165/view
